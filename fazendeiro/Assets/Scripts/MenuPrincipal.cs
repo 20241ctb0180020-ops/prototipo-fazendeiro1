67fa45d6bf36bddfb6da68ff5b7c9974af8b3630
Guilherme <20241ctb0180020@estudantes.ifpr.edu.br>
2026-04-06 22:10:35 -0300

    menu

---
+++ fazendeiro/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    [SerializeField] private string nomeFase;
+    [SerializeField] private GameObject MenuInicial;
+    [SerializeField] private GameObject MenuOpcoes;
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeFase);
+    }
+    public void AbrirOpcoes()
+    {
+        MenuInicial.SetActive(false);
+        MenuOpcoes.SetActive(true);
+    }
+    public void FecharOpcoes()
+    {
+        MenuOpcoes.SetActive(false);
+        MenuInicial.SetActive(true);
+    }
+    public void SairJogo()
+    {
+        Debug.Log("Saiu");
+        Application.Quit();
+    }
+}
